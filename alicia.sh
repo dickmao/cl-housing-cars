9d3d40bf6e35fb490880dbfc72884ab13819f997
dickmao <rchiang@cs.stonybrook.edu>
2017-07-21 10:36:06 -0400

    fix regex for finding old markers

--- alicia.sh
+++ alicia.sh
@@ -6,6 +6,9 @@ export PATH=/home/ubuntu/.local/bin:/home/ubuntu/.local/bin:/home/ubuntu/.go/bin
 cd ~/scrapy
 scrapy crawl $name 
 NAME=$name sh -c "jupyter nbconvert --ExecutePreprocessor.timeout=300 --stdout --execute dedupe.ipynb"
+if [ -e $name/marker1 ]; then 
+  mv $name/marker1 $name/marker0
+fi
 if [ -s "$name/digest" ]; then
   DATE=`date '+%Y%m%d'` sh -c "mailx -s \"digest \$DATE\" -r \"Daily Digest <no-reply@shunyet.com>\" -q $name/digest $mailto rchiang@cs.stonybrook.edu </dev/null"
 fi

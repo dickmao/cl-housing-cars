6732e3c9a94f3e4eb0d491e52b12ca85617c48f3
dickmao <rchiang@cs.stonybrook.edu>
2017-09-11 13:02:38 -0400

    don't needlessly narrow down que

--- tutorial/spiders/que_spider.py
+++ tutorial/spiders/que_spider.py
@@ -3,5 +3,5 @@ from BaseSpider import BaseSpider
 class DmozSpider(BaseSpider):
     name = "que"
     start_urls = [
-        "https://newyork.craigslist.org/search/que/sub?min_bedrooms=1&max_bedrooms=1&min_bathrooms=1&max_bathrooms=1&availabilityMode=0&private_room=1&private_bath=1",
+        "https://newyork.craigslist.org/search/que/sub?min_bedrooms=1&max_bedrooms=1&min_bathrooms=1&max_bathrooms=1",
     ]

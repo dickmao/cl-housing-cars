178cf2a096c1f471b98e0bec0c5a217a3db19355
dickmao <rchiang@cs.stonybrook.edu>
2017-09-12 03:10:02 -0400

    must specify private_bath for que

--- tutorial/spiders/que_spider.py
+++ tutorial/spiders/que_spider.py
@@ -3,5 +3,5 @@ from BaseSpider import BaseSpider
 class DmozSpider(BaseSpider):
     name = "que"
     start_urls = [
-        "https://newyork.craigslist.org/search/que/sub?min_bedrooms=1&max_bedrooms=2",
+        "https://newyork.craigslist.org/search/que/sub?min_bedrooms=1&max_bedrooms=2&private_room=1&private_bath=1",
     ]

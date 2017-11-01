b0164c11821b0dba49e79a765671e893d1126cc3
dickmao <rchiang@cs.stonybrook.edu>
2017-11-01 01:41:30 -0400

    revert pipelines.py since the json file doesn't exist yet at close_spider

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -5,11 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.extensions.feedexport import FeedExporter
-import logging
 import os
-from subprocess import Popen, PIPE, STDOUT
-
-logger = logging.getLogger(__name__)
 
 class SomePipeline(object):
     def __init__(self, exporter):
@@ -29,12 +25,3 @@ class SomePipeline(object):
         except OSError:
             pass
         os.symlink(os.path.basename(self.exporter.slot.file.name), slink)
-        dedupe_dir = os.path.dirname(spider.settings['FEED_URI']) % self.exporter._get_uri_params(spider)
-        dedupe_script = os.path.join(spider.settings['SCRIPT_DIR'], "dedupe.py")
-        logger.info("Running {} {}".format(dedupe_script, dedupe_dir))
-        # :stackoverflow: jfs
-        process = Popen([dedupe_script, dedupe_dir], stdout=PIPE, stderr=STDOUT)
-        with process.stdout:
-            for line in iter(process.stdout.readline, b''):
-                logging.info('%r', line)
-        process.wait()

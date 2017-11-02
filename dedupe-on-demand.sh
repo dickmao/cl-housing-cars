9e69eb941feb44549739c8f85a7b926adecefff2
dickmao <rchiang@cs.stonybrook.edu>
2017-11-02 10:17:42 -0400

    add target to dedupe-on-demand

--- dedupe-on-demand.sh
+++ dedupe-on-demand.sh
@@ -6,7 +6,7 @@ ITEMDIR=/var/lib/scrapyd/items/${PROJECT}
 
 
 while [ 1 ] ; do
-    for spider in $(scrapyd-client spiders -p ${PROJECT} | tail -n +2) ; do
+    for spider in $(scrapyd-client -t http://scrapyd:6800 spiders -p ${PROJECT} | tail -n +2) ; do
         if [ -e ${ITEMDIR}/${spider}/digest ] && [[ ! -z $(find -L ${ITEMDIR}/${spider} -name Marker.*\.json -cnewer ${ITEMDIR}/${spider}/digest) ]]; then
             ${WDIR}/dedupe.py ${ITEMDIR}/${spider}
         fi

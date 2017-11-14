4453d79383a12c233be8fc6b4f4550f51663f751
dickmao <rchiang@cs.stonybrook.edu>
2017-11-13 20:09:28 -0500

    fix dedupe-on-demand.sh logical operators

--- dedupe-on-demand.sh
+++ dedupe-on-demand.sh
@@ -4,11 +4,12 @@ WDIR=$(dirname $0)
 PROJECT=tutorial
 ITEMDIR=/var/lib/scrapyd/items/${PROJECT}
 
-
 while [ 1 ] ; do
     for spider in $(scrapyd-client -t http://scrapyd:6800 spiders -p ${PROJECT} | tail -n +2) ; do
-        if [ -e ${ITEMDIR}/${spider}/digest ] && [[ ! -z $(find -L ${ITEMDIR}/${spider} -name Marker.*\.json -cnewer ${ITEMDIR}/${spider}/digest) ]]; then
-            ${WDIR}/dedupe.py ${ITEMDIR}/${spider}
+        if [ -d ${ITEMDIR}/$spider ]; then
+           if ( [ ! -e ${ITEMDIR}/${spider}/digest ] && [ ! -z "$(find -L ${ITEMDIR}/$spider -name 'Marker.*\.json')" ] ) || ( [ -e ${ITEMDIR}/${spider}/digest ] && [[ ! -z $(find -L ${ITEMDIR}/${spider} -name 'Marker.*\.json' -cnewer ${ITEMDIR}/${spider}/digest) ]] ); then
+               ${WDIR}/dedupe.py ${ITEMDIR}/${spider}
+           fi
         fi
     done
     sleep 30

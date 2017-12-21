fc489654d08e4c77f02ef186f9ce85d9667b185f
dickmao <rchiang@cs.stonybrook.edu>
2017-12-21 16:41:17 -0500

    get fit.py runnable

--- fit.py
+++ fit.py
@@ -1,12 +1,9 @@
 from __future__ import division, print_function
 import logging
 
-from common_utils.s3_utils import download_file, upload_file, download_folder, upload_folder
-from common_utils.job_utils import generate_working_dir, delete_working_dir
-
 from time import time
 import os, errno, operator, re, sys, subprocess, signal, redis
-import itertools, shutil, requests
+import itertools, shutil, requests, boto3, pickle
 
 from corenlp import CoreNLPClient, WordTokenizer, SentTokenizer
 import numpy as np
@@ -93,13 +90,16 @@ class CallableAnnotate(Callable):
 def get_text_length(docs):
     return np.array([len(doc) for doc in docs]).reshape(-1, 1)
 
-def count_pronouns(docs):
-    result = []
-    for doc in docs:
-        ann = client.annotate(doc, annotators="ner".split())
-        result.append(sum(itertools.chain.from_iterable([[1 for t in s.token if (t.pos == "PRP" or t.pos == "PRP$")] for s in ann.sentence])))
-    return np.asarray(result).reshape(-1, 1)
+class CallableCountPronouns(Callable):
+    def __init__(self, client):
+        self._client = client
 
+    def __call__(self, docs):
+        result = []
+        for doc in docs:
+            ann = self._client.annotate(doc, annotators="ner".split())
+            result.append(sum(itertools.chain.from_iterable([[1 for t in s.token if (t.pos == "PRP" or t.pos == "PRP$")] for s in ann.sentence])))
+        return np.asarray(result).reshape(-1, 1)
 
 def main():
     argparser = argparse.ArgumentParser()
@@ -107,18 +107,10 @@ def main():
     # argparser.add_argument('--folders', nargs='*', type=str, help='s3 paths')
     # argparser.add_argument('--files', nargs='*', type=str, help='s3 paths')
     # argparser.add_argument('exec', type=str, help='s3 path')
-    argparser.add_argument('--working-dir', type=argparse_dirtype, default='/')
-    argparser.add_argument('--corenlp-host', type=str, default='http://localhost:9005')
-    args = parser.parse_args()
-
-    os.environ['CORENLP_HOME'] = "/CoreNLP"
+    argparser.add_argument('--working-dir', type=argparse_dirtype, default='.')
+    argparser.add_argument('--corenlp-uri', type=str, default='http://localhost:9005')
+    args = argparser.parse_args()
     os.chdir(args.working_dir)
-
-    for path in args.folders:
-        download_folder(path, ".")
-
-    for path in args.files:
-        download_file(path, ".")
 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket("303634175659.newyork")
@@ -151,34 +143,41 @@ def main():
         else:
             break
 
-    client = CoreNLPClient(start_cmd="gradle -p {} server".format(os.getenv("CORENLP_HOME")), endpoint=args.corenlp_uri, timeout=15000)
-    cr = Json100CorpusReader(wdir, jsons, dedupe="id", word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
-    crabo = Json100CorpusReader(wdir, jsons, dedupe="id", link_select='abo', word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
-    crsub = Json100CorpusReader(wdir, jsons, dedupe="id", link_select='sub', word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
-    crjoin = Json100CorpusReader(wdir, ["joinery.json"], dedupe="id", word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
-    crlp = Json100CorpusReader(wdir, ["listingsproject.json"], dedupe="id", word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
+    with CoreNLPClient(start_cmd="gradle -p {} server".format("../CoreNLP"), endpoint=args.corenlp_uri, timeout=15000) as client:
+        cr = Json100CorpusReader(".", jsons, dedupe="id", word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
+        crabo = Json100CorpusReader(".", jsons, dedupe="id", link_select='abo', word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
+        crsub = Json100CorpusReader(".", jsons, dedupe="id", link_select='sub', word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
+        crjoin = Json100CorpusReader(".", ["joinery.json"], dedupe="id", word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
+        crlp = Json100CorpusReader(".", ["listingsproject.json"], dedupe="id", word_tokenizer=WordTokenizer(client), sent_tokenizer=SentTokenizer(client))
 
-    crtrain_desc = list(itertools.chain(crabo.field('desc'), crlp.field('desc')))
-    crtrain_labels = [-1] * len(crabo) + [1] * len(crlp)
-    crtest_desc = list(itertools.chain(crsub.field('desc'), crjoin.field('desc')))
-    crtest_labels = [-1] * len(crsub) + [1] * len(crjoin)
+        crtrain_desc = list(itertools.chain(crabo.field('desc'), crlp.field('desc')))
+        crtrain_labels = [-1] * len(crabo) + [1] * len(crlp)
+        crtest_desc = list(itertools.chain(crsub.field('desc'), crjoin.field('desc')))
+        crtest_labels = [-1] * len(crsub) + [1] * len(crjoin)
 
-    features = FeatureUnion([
-            ('text', Pipeline([
-                ('vectorizer', CountVectorizer(max_df=0.5, min_df=2, analyzer=CallableAnnotate(client))),
-                ('tfidf', TfidfTransformer()),
-            ])),
-            ('length', Pipeline([
-                ('count', FunctionTransformer(get_text_length, validate=False)),
-            ])),
-            ('pronouns', Pipeline([
-                ('count', FunctionTransformer(count_pronouns, validate=False)),
-            ])),
-    ])
+        features = FeatureUnion([
+                ('text', Pipeline([
+                    ('vectorizer', CountVectorizer(max_df=0.5, min_df=2, analyzer=CallableAnnotate(client))),
+                    ('tfidf', TfidfTransformer()),
+                ])),
+                ('length', Pipeline([
+                    ('count', FunctionTransformer(get_text_length, validate=False)),
+                ])),
+                ('pronouns', Pipeline([
+                    ('count', FunctionTransformer(CallableCountPronouns(client), validate=False)),
+                ])),
+        ])
 
-    svc = make_pipeline(features, SVC(C=10,kernel='linear'), memory=sklearn.externals.joblib.Memory(cachedir="/var/tmp", verbose=0))
-    svc.fit(crtrain_desc, crtrain_labels)
-    svc.score(crtest_desc, crtest_labels)
+        # pickling with count_pronouns will defeat:
+        # memory=sklearn.externals.joblib.Memory(cachedir="/var/tmp", verbose=0)
+        # svc = make_pipeline(features, SVC(C=10,kernel='linear'), memory=None)
+        svc = make_pipeline(features, SVC(C=10,kernel='linear'), memory=sklearn.externals.joblib.Memory(cachedir="/var/tmp", verbose=0))
+        svc.fit(crtrain_desc, crtrain_labels)
+        y_pred = svc.predict(crtest_desc)
+        print(metrics.accuracy_score(crtest_labels, y_pred))
+        print(metrics.confusion_matrix(crtest_labels, y_pred))
+        sklearn.externals.joblib.dump(svc.named_steps['svc'], 'fit.pkl')
+        
 
 if __name__ == '__main__':
     main()

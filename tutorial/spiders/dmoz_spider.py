93b74902cd28ffe1c8094625f665434e5d45a75e
dickmao <rchiang@cs.stonybrook.edu>
2018-01-05 22:28:33 -0500

    change dmoz to newyork

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -1,7 +1,7 @@
 from .CraigSpider import CraigSpider
 
 class DmozSpider(CraigSpider):
-    name = "dmoz"
+    name = "newyork"
     start_urls = [
         "https://newyork.craigslist.org/search/mnh/abo?min_bedrooms=0&max_bedrooms=3&min_price=500&max_price=5000&private_room=1&private_bath=1",
         "https://newyork.craigslist.org/search/mnh/sub?min_bedrooms=0&max_bedrooms=3&min_price=500&max_price=5000&private_room=1&private_bath=1",
